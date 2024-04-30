6997252297b2c38e43581bb40315d9221aeea35a
Cobalttt2311 <152157640+Cobalttt2311@users.noreply.github.com>
2024-04-30 11:28:41 +0700

    Update headerfile.h

--- headerfile.h
+++ headerfile.h
@@ -1,4 +1,23 @@
 #ifndef HEADERFILE_H
 #define HEADERFILE_H
 
+
+
+typedef struct nodeCongklak {
+	char namaLubang;
+	char pemilikLubang;
+	int jumlahbiji;
+	struct nodeCongklak *berikut;
+	struct nodeCongklak *seberang;
+	struct nodeLumbung *lumbung;
+} nodeCongklak;
+
+typedef struct nodeLumbung {
+	char namaLumbung;
+	char pemilikLumbung;
+	int jumlahbiji;
+	struct nodeCongklak *berikut;
+} nodeLumbung;
+
+
 #endif
