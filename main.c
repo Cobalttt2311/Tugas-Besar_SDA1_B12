3fe495b24124e5bc8f223729513aac7797f19a26
Cobalttt2311 <152157640+Cobalttt2311@users.noreply.github.com>
2024-05-14 08:59:50 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "congklak.h"
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include "headerfile.h"
 
 int main() {
+    int pil;
     nodeCongklak board[14];
     char pilihan;
-    makeBoard(board);
+    int boardInitialized = 0;
 
     while (1) {
-        displayBoard(board);
-        printf("Pilih lubang (A-N) atau X untuk keluar: ");
-        scanf(" %c", &pilihan);
+        system("cls"); // Clear the console screen
+        printf("=========================================\n");
+        printf("|           GAME CONGKLAK               |\n");
+        printf("|---------------------------------------|\n");
+        printf("|1. Mulai Permainan                     |\n");
+        printf("|2. Tampilkan Skor                      |\n");
+        printf("|3. Cara Bermain                        |\n");
+        printf("|4. Keluar Permainan                    |\n");
+        printf("|                                       |\n");
+        printf("=========================================\n");
+        printf("Masukan Pilihan Anda (1-4) :\n");
+        scanf("%d", &pil);
 
-        if (pilihan == 'X' || pilihan == 'x') {
-            break;
+        switch (pil) {
+            case 1:
+                system("cls");
+                if (!boardInitialized) {
+                    makeBoard(board);
+                    boardInitialized = 1;
+                }
+                
+                while (1) {
+                    displayBoard(board);
+                    printf("Pilih lubang (A-N) atau X untuk keluar: ");
+                    scanf(" %c", &pilihan);
+
+                    if (pilihan == 'X' || pilihan == 'x') {
+                        break;
+                    }
+
+                    moveSeed(board, pilihan);
+                }
+                break;
+
+            case 2:
+                system("cls");
+                printf("Masih dalam pengembangan\n");
+                break;
+                
+            case 3:
+                system("cls");
+                printf("Masih dalam pengembangan\n");
+                break;
+
+            case 4:
+                printf("Keluar dari permainan....\n");
+                // No need to free the static array
+                return 0;
+
+            default:
+                printf("Pilihan tidak valid. Silakan masukkan pilihan antara 1-4.\n");
+                break;
         }
-
-        moveSeed(board, pilihan);
+        printf("Press any key to continue...\n");
+        getchar(); 
+        getchar(); 
     }
 
     return 0;
