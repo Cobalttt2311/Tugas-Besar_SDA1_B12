70423de3e286757e65270ed5cf694bc830dddc5e
Muammar Syahid Rahmallah <143985336+syahiddrr@users.noreply.github.com>
2024-05-14 08:34:22 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "headerfile.h"
+#include "congklak.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	// Membuat papan permainan
-    nodeCongklak *board = (nodeCongklak *)malloc(sizeof(nodeCongklak));
+    nodeCongklak board[14];
+    char pilihan;
     makeBoard(board);
 
-    // Menampilkan papan permainan
-    displayBoard(board);
+    while (1) {
+        displayBoard(board);
+        printf("Pilih lubang (A-N) atau X untuk keluar: ");
+        scanf(" %c", &pilihan);
 
-    // Dealokasi memori papan permainan
-    free(board);
+        if (pilihan == 'X' || pilihan == 'x') {
+            break;
+        }
+
+        moveSeed(board, pilihan);
+    }
 
     return 0;
 }
