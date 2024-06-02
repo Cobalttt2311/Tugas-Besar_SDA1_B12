6b2a8010d20c05b815afc5d93eb2987eccaff6e6
Cobalttt2311 <152157640+Cobalttt2311@users.noreply.github.com>
2024-06-02 13:43:41 +0700

    Update headerfile.h

--- headerfile.h
+++ headerfile.h
@@ -14,7 +14,7 @@ typedef struct nodeCongklak {
     int jumlahbiji;
     struct nodeCongklak *berikut;
     struct nodeCongklak *seberang;
-    nodeLumbung *lumbung;
+    struct nodeLumbung *lumbung;
 } nodeCongklak;
 
 // Fungsi untuk membuat papan congklak ( alokasi memori buat congklak )
@@ -31,9 +31,6 @@ int isValidMove(nodeCongklak *board, char lubang, char player);
 
 // Fungsi untuk memeriksa sisi yang kosong
 int checkEmptySide(nodeCongklak *board, char player);
-
-// Fungsi untuk menangkap biji-biji lawan setelah pemain mengakhiri gilirannya di lubang yang berisi satu biji
-//void captureSeeds(nodeCongklak *board, char lubang);
 
 // Fungsi untuk memeriksa apakah terdapat pemenang dalam permainan congklak
 int checkWinner(nodeCongklak *board);
@@ -52,11 +49,5 @@ void saveGame(nodeCongklak *board, const char *filename);
 
 // Fungsi untuk memuat status permainan dari file eksternal
 void loadGame(nodeCongklak *board, const char *filename);
-
-// Fungsi untuk menampilkan petunjuk cara bermain congklak ke layar
-void displayInstructions();
-
-// Fungsi untuk menampilkan menu permainan congklak ke layar
-void displayMenu();
 
 #endif // HEADERFILE_H
